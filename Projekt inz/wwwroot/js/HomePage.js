68f26c8f2f600fa8dbd8375819de992a3d2f42b2
Wojtorus <wojtekostrouch@gmail.com>
2024-08-28 22:16:51 +0200

    Initial commit

---
+++ Projekt inz/wwwroot/js/HomePage.js
@@ -0,0 +1,19 @@
+﻿let moon = document.getElementById("moon");
+let text = document.getElementById("text");
+let cityRight = document.getElementById("ciyRight");
+let cityLeft = document.getElementById("cityLeft");
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+    let screenWidth = window.innerWidth;
+    moon.style.top = value * .75 + "px"
+    cityLeft.style.left = value * -1.5 + "px";
+    cityRight.style.left = value * 1.5 + "px";
+
+    if (screenWidth > 1356) {
+        text.style.marginTop = value * 1.5 + "px";
+    }
+    else {
+        text.style.marginTop = value * .7 + "px";
+    }
+});
